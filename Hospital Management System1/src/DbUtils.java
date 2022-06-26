0ec070696a52fd3d655b70a2b58c7de199b961a1
suprava1118 <104939704+suprava1118@users.noreply.github.com>
2022-06-27 00:41:32 +0530

    Add files via upload

---
+++ Hospital Management System1/src/DbUtils.java
@@ -0,0 +1,20 @@
+
+import java.sql.ResultSet;
+import javax.swing.table.TableModel;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author suprava
+ */
+class DbUtils {
+
+    static TableModel resultSetToTableModel(ResultSet rs) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
